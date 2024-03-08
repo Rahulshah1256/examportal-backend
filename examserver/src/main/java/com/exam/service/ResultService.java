f94bb845aac2bea1c507c6e410870207f0f218b3
RahulShah1256 <rahul.kumar@neebal.com>
2024-03-08 11:20:37 +0530

    examportal-backend

---
+++ examserver/src/main/java/com/exam/service/ResultService.java
@@ -0,0 +1,21 @@
+package com.exam.service;
+
+import java.util.List;
+
+import com.exam.model.User;
+import com.exam.model.exam.Quiz;
+import com.exam.model.exam.Result;
+
+public interface ResultService {
+	
+	public Result addResult(Result result);
+	
+	public List<Result> getAllResult();
+	
+	public List<Result> getResultOfQuiz(Quiz quiz);
+	
+	public List<Result> getResultOfUser(User user);
+	
+	public List<Result> getResultOfUserAndQuiz(Quiz quiz,User user);
+
+}
